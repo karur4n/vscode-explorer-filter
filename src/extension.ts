dae11ea741c6dec6a671f4d682fa35fb7e344cf6
Kazuki Furukawa <6816398+karur4n@users.noreply.github.com>
2025-09-25 22:10:28 +0900

    update

---
+++ src/extension.ts
@@ -0,0 +1,36 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable1 = vscode.commands.registerCommand(
+    "filter-folder.filter",
+    async () => {
+      vscode.commands.executeCommand("list.find");
+    }
+  );
+
+  context.subscriptions.push(disposable1);
+
+  const disposable2 = vscode.commands.registerCommand(
+    "filter-folder.reveal",
+    async () => {
+      await vscode.commands.executeCommand("copyFilePath");
+      const filePath = await vscode.env.clipboard.readText();
+      const fileUri = vscode.Uri.file(filePath);
+
+      await vscode.commands.executeCommand("list.closeFind");
+
+      // Adjusted value based on actual testing
+      await sleep(300);
+
+      vscode.commands.executeCommand("revealInExplorer", fileUri);
+    }
+  );
+
+  context.subscriptions.push(disposable2);
+}
+
+export function deactivate() {}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
